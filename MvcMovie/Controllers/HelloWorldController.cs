a6d9bc994a54b9b984bb5cc9cc03a36d2bd28465
chenkang216 <chenkang216@gmail.com>
2015-03-18 01:07:22 +0800

    Create solution

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "This is index action.";
+        }
+
+        public string HelloWorld()
+        {
+            return "This is hello world action.";
+        }
+
+        public string Welcome(string name, int id = 1)
+        {
+            return HttpUtility.HtmlEncode(string.Format("Hello {0}, numTimes is {1}", name, id));
+        }
+    }
+}
